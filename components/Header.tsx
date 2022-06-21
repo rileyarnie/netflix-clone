508f3b4fe498fd2f393e2049ca58ba780b22ed21
Arnold Kibira <arnold.ril3y@gmail.com>
2022-06-21 15:30:56 +0300

    style the header to make it responsive

---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+import logo from "../public/Logonetflix.png";
+import { BellIcon, SearchIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import avatar from "../public/avatar.png";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <Image
+          src={logo}
+          alt="Logo"
+          className="cursor-pointer object-contain"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New and Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      <div className="flex items-center text-sm space-x-4 font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account" className="">
+          <div className="">
+            <Image src={avatar} alt="avatar" className="cursor-pointer ml-4" />
+          </div>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
