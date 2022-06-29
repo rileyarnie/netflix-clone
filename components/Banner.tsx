10ceb48e53f6240f4b1ccefa48bd41d484019a57
Arnold Kibira <arnold.ril3y@gmail.com>
2022-06-29 17:30:14 +0300

    create banner component

---
+++ components/Banner.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+        <div className="">
+            {/* <Image/> */}
+        </div>
+    </div>
+  )
+}
+
+export default Banner
