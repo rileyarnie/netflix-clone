ed866cc25904182b8bd7587f08c04822d038bdac
Arnold Kibira <arnold.ril3y@gmail.com>
2022-07-01 23:07:57 +0300

    create banner component that dynamically displays movie information

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,14 +1,48 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { baseUrl } from "../constants/movie";
+import { Movie } from "../intefaces";
+import { FaPlay } from "react-icons/fa";
+import { InformationCircleIcon } from "@heroicons/react/solid";
 
-const Banner = () => {
-  return (
-    <div>
-        <div className="">
-            {/* <Image/> */}
-        </div>
-    </div>
-  )
+interface Props {
+  netflixOriginals: Movie[];
 }
+const Banner = ({ netflixOriginals }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null);
 
-export default Banner
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
+
+  return (
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+      <div className="absolute top-0 left-0 h-[95vh] w-screen -z-10">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+          objectFit="cover"
+          alt={movie?.title || movie?.name || movie?.original_name}
+        />
+      </div>
+      <h1 className="text-2xl md:text-4xl lg:text-7xl font-bold">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
+      <div className="flex space-x-3">
+        <button className="bannerButton bg-white text-black">
+          <FaPlay className="h-4 w-4 text-black md:h-8 md:w-8" /> Play
+        </button>
+        <button className="bannerButton bg-[gray]/70">
+          More Info <InformationCircleIcon className="h-4 w-4 md:h-8 md:w-8" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
